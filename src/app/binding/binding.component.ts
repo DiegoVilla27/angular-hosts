cc3c277c8432cbe09f8fbfe5acf317fff2a7350f
Diego Villa <dv270992@gmail.com>
2023-11-15 23:54:31 +0100

    feat: Implement HostBindign example

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 
 @Component({
   selector: "app-binding",
   templateUrl: "./binding.component.html",
   styleUrls: ["./binding.component.scss"]
 })
-export class BindingComponent {}
+export class BindingComponent {
+
+  @HostBinding('style.color') color = 'blue';
+  @HostBinding('attr.example-attr') attr = 'value-attr';
+  // This class is declared in styles.scss
+  @HostBinding('class.badge') badgeClass = true;
+}
